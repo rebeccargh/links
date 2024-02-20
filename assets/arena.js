088bf09667568f3f797067ad3da8ea93e861805c
Rebecca H <hartr693@newschool.edu>
2024-02-20 17:20:17 -0500

    Added hover cursor & intersection observer.....
    Observer functions but does not animate correctly yet. Animation to come.

--- assets/arena.js
+++ assets/arena.js
@@ -223,4 +223,26 @@ fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-stor
 		let channelUsers = document.getElementById('channel-users') // Show them together
 		data.collaborators.forEach((collaborator) => renderUser(collaborator, channelUsers))
 		renderUser(data.user, channelUsers)
+
+		let highlightClass = 'breath1' // Variables again.
+		let highlightBlocks = document.querySelectorAll('li') // Get all of them.
+		
+		// Loop through the list, doing this `forEach` one.
+		highlightBlocks.forEach((block) => {
+			let sectionObserver = new IntersectionObserver((entries) => {
+				let [entry] = entries
+		
+				if (entry.isIntersecting) {
+					block.classList.add(highlightClass)
+				} else {
+					block.classList.remove(highlightClass)
+				}
+			}, {
+				root: document, // This is only needed in the example iframe!
+				rootMargin: '-20% 0% -20% 0%', // CSS-ish: top/right/bottom/left.
+			})
+		
+			sectionObserver.observe(block) // Watch each one!
+		})
 	})
+
